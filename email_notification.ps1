178609491c33cab8fdf161adc7d0c725f21c2c88
Amr Elsayyad <amrelsayyad96@gmail.com>
2021-07-22 10:47:08 +0200

    Create email_notification.ps1
    A PowerShell script used for email notification when the data collection is done.

---
+++ email_notification.ps1
@@ -0,0 +1,50 @@
+﻿$credential = Get-Credential -Message "Enter your outlook email credentials:"
+
+$TestParams = @{
+
+    From = "$((gwmi Win32_ComputerSystem).Model) <$($credential.UserName)>"
+    To = "You <$($credential.UserName)>"
+    Subject = 'Notification Service Started'
+    Body = "`nEmail notification service on $((gwmi Win32_ComputerSystem).Model) has started.`n`nTime: $(Get-Date)`n"
+    SMTPServer = 'smtp-mail.outlook.com'
+    Port = 587
+    UseSsl = $true
+    Credential = $credential
+
+}
+
+Send-MailMessage @TestParams -ErrorAction Stop
+
+$size = (Get-Item "$($HOME)\Documents\GitHub\Medium-Articles-Data-Analysis\Medium_Data.csv").length
+
+while(1) {
+
+    Start-Sleep -s 60
+    
+    if($size -eq (Get-Item "$($HOME)\Documents\GitHub\Medium-Articles-Data-Analysis\Medium_Data.csv").length) {
+
+                    
+        $MailParams = @{
+
+            From = "$((gwmi Win32_ComputerSystem).Model) <$($credential.UserName)>"
+            To = "You <$($credential.UserName)>"
+            Subject = 'Data Collection Ended'
+            Body = "`nData collection on $((gwmi Win32_ComputerSystem).Model) has ended.`n`nTime: $(Get-Date)`n"
+            SMTPServer = 'smtp-mail.outlook.com'
+            Port = 587
+            UseSsl = $true
+            Credential = $credential
+
+        }
+
+        Send-MailMessage @MailParams -ErrorAction Stop
+
+        break
+            
+    } else {
+
+        $size = (Get-Item "$($HOME)\Documents\GitHub\Medium-Articles-Data-Analysis\Medium_Data.csv").length
+
+    }
+
+}
