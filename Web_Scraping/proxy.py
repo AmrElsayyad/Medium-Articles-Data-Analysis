d4c438ef8e22119cea88e11c758bdeded5a798aa
Amr Elsayyad <amrelsayyad96@gmail.com>
2021-07-22 21:42:26 +0200

    Update proxy.py
    Added the next page of proxies to the proxies list. Total of 40 proxies.

--- Web_Scraping/proxy.py
+++ Web_Scraping/proxy.py
@@ -19,34 +19,48 @@ def get_proxies():
     options.headless = True
     options.add_argument("--disable-extensions")
     browser = webdriver.Firefox(options=options)
-    time.sleep(5)   # Wait for browser to open
+    time.sleep(5)  # Wait for browser to open
     browser.get(URL)
 
-    # Sort elements by https
-    sort_https = browser.find_element_by_xpath("//th[@aria-label='Https: activate to sort column ascending']")
-    sort_https.click()
-    sort_https.click()
+    def get_table():
+        """
+        Get Proxies list in page
+        """
+        # Sort elements by https
+        sort_https = browser.find_element_by_xpath("//th[@aria-label='Https: activate to sort column ascending']")
+        sort_https.click()
+        sort_https.click()
 
-    # Getting table
-    table = browser.find_element_by_xpath("//table[@aria-describedby='proxylisttable_info']")
+        # Getting table
+        table = browser.find_element_by_xpath("//table[@aria-describedby='proxylisttable_info']")
 
-    # Getting td elements
-    tds = table.find_elements_by_tag_name('td')
+        # Getting td elements
+        tds = table.find_elements_by_tag_name('td')
 
-    # Getting IP Address and Port
-    for i in range(0, len(tds) - 1, 8):
-        # Each row has 8 elements we only want the first 2
-        ip = tds[i].text
-        port = tds[i + 1].text
+        # Getting IP Address and Port
+        for i in range(0, len(tds) - 1, 8):
+            # Each row has 8 elements we only want the first 2
+            ip = tds[i].text
+            port = tds[i + 1].text
 
-        # Getting Proxy
-        proxy = {
-            "http": 'http://' + ip + ':' + port,
-            "https": 'https://' + ip + ':' + port
-        }
+            # Getting Proxy
+            proxy = {
+                "http": 'http://' + ip + ':' + port,
+                "https": 'https://' + ip + ':' + port
+            }
 
-        # Appending proxy to proxies list
-        proxies.append(proxy)
+            # Appending proxy to proxies list
+            proxies.append(proxy)
+
+    # Getting proxies in the first page
+    get_table()
+
+    # Going to the next page
+    next_page = browser.find_element_by_xpath("//*[@id='proxylisttable_next']/a")
+    next_page.click()
+
+    # Getting proxies in the second page
+    get_table()
 
     # Close browser
     browser.quit()
